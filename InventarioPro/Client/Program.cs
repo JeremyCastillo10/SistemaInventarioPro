baf007077cdd69bbccefdaee648883b40e663c1c
jeremy castillo taveras <jeremycastillo901@gmail.com>
2024-11-01 14:55:46 -0400

    Dashboard

--- InventarioPro/Client/Program.cs
+++ InventarioPro/Client/Program.cs
@@ -3,6 +3,7 @@ using InventarioPro.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,4 +12,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddSweetAlert2();
+builder.Services.AddRadzenComponents();
 await builder.Build().RunAsync();
